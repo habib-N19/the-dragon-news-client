587174b6eb22d0192dfe313e1559e7dfd08d18bb
habib-N19 <habiburnabiarafat@gmail.com>
2023-04-27 17:22:55 +0600

    added dragon news home page main content

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useLoaderData, useParams } from 'react-router-dom'
+import NewsCard from '../Home/NewsCard/NewsCard'
+
+const Category = () => {
+  // to show dynamically the id name and info in dynamic routes
+  const { id } = useParams()
+  //   retriving data dynamically from loader
+  const categoryNews = useLoaderData()
+
+  return (
+    <div>
+      <h2>this is a category news: {categoryNews.length}</h2>
+      {categoryNews.map(news => (
+        <NewsCard key={news._id} news={news}></NewsCard>
+      ))}
+    </div>
+  )
+}
+
+export default Category
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import logo from '../../../assets/logo.png'
 import moment from 'moment'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 import Marquee from 'react-fast-marquee'
 const Header = () => {
   return (
-    <Container>
+    <Container className='mt-4'>
       <div className='text-center'>
         <img src={logo} alt='' />
         <p className='text-secondary'>
@@ -13,19 +13,36 @@ const Header = () => {
         </p>
         <p>{moment().format('dddd, MMMM D, YYYY')}</p>
       </div>
-      <div>
+      <div className='d-flex'>
         <Button variant='danger'>Latest</Button>
         <Marquee
           pauseOnClick={true}
           speed={60}
-          className='text-danger'
-          gradient={true}
-          gradientColor={[255, 155, 55]}
+          pauseOnHover={true}
+          className='text-danger '
         >
           I can be a React component, multiple React components, or just some
           text.
         </Marquee>
       </div>
+      <Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
+        <Container>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mx-auto'>
+              <Nav.Link href='#features'>Home</Nav.Link>
+              <Nav.Link href='#pricing'>About</Nav.Link>
+              <Nav.Link href='#career'>Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href='#deets'>Profile</Nav.Link>
+              <Nav.Link eventKey={2} href='#memes'>
+                <Button variant='dark'>Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   )
 }
