7f99f7fce4aafdc88072f17de9e261f8858fbab6
habib-N19 <habiburnabiarafat@gmail.com>
2023-04-28 16:58:02 +0600

    added auth context

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -10,7 +10,7 @@ const Category = () => {
 
   return (
     <div>
-      <h2>this is a category news: {categoryNews.length}</h2>
+      {id && <h2>this is a category news: {categoryNews.length}</h2>}
       {categoryNews.map(news => (
         <NewsCard key={news._id} news={news}></NewsCard>
       ))}
